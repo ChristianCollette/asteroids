cabea8aab233dea69bcdf2bdc2f837c064b3d99c
Christian Collette <christianhcollette@gmail.com>
2025-01-07 22:37:12 -0600

    Rendering player ship

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from constants import *
+import player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player_character = player.Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    dt = 0
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(color=(0, 0, 0))
+        player_character.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)/1000)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
